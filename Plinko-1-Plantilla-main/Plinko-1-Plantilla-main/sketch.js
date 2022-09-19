903143af2b64bb022d66a1a4b0184acfd3879628
sans203 <92894802+sans203@users.noreply.github.com>
2022-09-19 12:59:49 -0500

    Add files via upload

---
+++ Plinko-1-Plantilla-main/Plinko-1-Plantilla-main/sketch.js
@@ -0,0 +1,114 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+  //crea los objetos para dividir
+  for (var k = 0; k <=80; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for (var k = 0; k <=160; k = k + 160) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for (var k = 0; k <=240; k = k + 240) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  
+  for (var k = 0; k <=320; k = k + 320) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for (var k = 0; k <=400; k = k + 400) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for (var k = 0; k <=480; k = k + 480) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for (var k = 0; k <=560; k = k + 560) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for (var k = 0; k <=640; k = k + 640) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for (var k = 0; k <=720; k = k + 720) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  for (var k = 0; k <=800; k = k + 800) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+
+  for (var k = 0; k <=880; k = k + 880) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  //crea la primera fila de objetos plinko
+  for (var j = 75; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,75));
+  }
+
+  //crea la segunda fila de objetos plinko
+  for (var j = 50; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,175));
+  }
+
+  //crea la tercera fila de objetos plinko
+  for (var j = 25; j <=width-20; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,275));
+  }
+  
+  //crea la cuarta fila de objetos plinko
+  for (var j = 0; j <=width-30; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,375));
+  }
+
+  //crea los objetos partícula
+  if(frameCount%90===0){
+    particles.push(new Particle(random(width/2+10), 10.10));
+  }
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ 
+  Engine.update(engine);
+  ground.display();
+  
+  //muestra los plinkos 
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();   
+  }
+   
+  //muestra las divisiones
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display();
+  }
+
+  //muestra las partículas 
+  particles.display();
+}
